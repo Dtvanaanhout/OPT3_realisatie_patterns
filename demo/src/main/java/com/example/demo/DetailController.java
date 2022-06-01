53d3716f18b65f3c40325dc0259919609051955b
Dtvanaanhout <demy.van.aanhout@gmail.com>
2022-06-01 11:28:08 +0200

    11

--- demo/src/main/java/com/example/demo/DetailController.java
+++ demo/src/main/java/com/example/demo/DetailController.java
@@ -1,17 +1,12 @@
 package com.example.demo;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
 
 public class DetailController {
     OverzichtController OC = new OverzichtController();
-    Product product = OC.producten.get(OC.lijstProducten.getSelectionModel().getSelectedIndex());
-    @FXML
-    private ResourceBundle resources;
+    Product product = OC.product;
 
-    @FXML
-    private URL location;
 
     @FXML
     void initialize() {
